9413d3e187e39793ac7e5c26bd12f3c18a44ea54
Luis Louro <lapisdecor@gmail.com>
2017-07-14 21:51:27 +0100

    use os.environ

--- hellosound/hellosound.py
+++ hellosound/hellosound.py
@@ -3,9 +3,11 @@
 # This was made to test a simple sound on pulseaudio in a snap
 
 import subprocess
+import os
 
 def playme():
-    """ Play a sound from freedesktop.org """
-    soundfile = "$SNAP/usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga"
+    """ Play a sound from freedesktop.org ""
+    
+    soundfile = os.environ["SNAP"] + "/usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga"
 
     subprocess.Popen(["paplay", soundfile])
